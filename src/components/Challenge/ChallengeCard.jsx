72af2107d9ab2dbc3a13acbe6849c3dd4d1f96ce
RyanFurt12 <ryanfurtadinho9@gmail.com>
2024-12-09 10:33:30 -0300

    Feat: Add Challenge Card

---
+++ src/components/Challenge/ChallengeCard.jsx
@@ -0,0 +1,12 @@
+import './ChallengeCard.css'
+
+export default function ChallengeCard() {
+  return (
+    <>
+        <a href='/desafio/[id]' className="card-container">
+            <h3>Challenge Name</h3>
+            <p>até 25/12/2024</p>
+        </a>
+    </>
+  );
+}
--- src/components/home/Challenges.jsx
+++ src/components/home/Challenges.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import ChallengeCard from '../Challenge/ChallengeCard';
 import './Challenges.css'
 
 export default function Challenges() {
@@ -8,30 +9,29 @@ export default function Challenges() {
       <div className="current-challenges">
         <h2>Seus Desafios</h2>
         <div className="challenges-scroll">
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
         </div>
       </div>
 
       <div className="old-challenges">
         <h2>Desafios Concluidos</h2>
         <div className="challenges-carroussel">
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
+          <ChallengeCard/>
         </div>
       </div>
 
