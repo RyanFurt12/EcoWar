352bd83ded7b03efe7735f2aa49d96fd02410f3c
RyanFurt12 <ryanfurtadinho9@gmail.com>
2024-12-09 20:43:46 -0300

    feat: add post page

--- src/app/desafio/[id]/page.jsx
+++ src/app/desafio/[id]/page.jsx
@@ -1,14 +1,65 @@
+import './desafio.css'
+import ReturnSvg from '@/components/svg/ReturnSvg';
+import PlusSvg from '@/components/svg/PlusSvg';
+
 export const metadata = {
   title: "Desafios - id",
-  description: "Página para a criação dos desafios",
+  description: "Página de visualização dos desafios",
 };
 
-export default async function Home({ params }) {
+export default async function DesafioPage({ params }) {
   const { id } = await params;
 
   return (
-    <div>
-      <h1>Desafios - id: {id}</h1>
+    <div className='challenge-container'>
+      <div className="header">
+        <a href="/"><ReturnSvg/></a>
+        <h1>Challenge Name</h1>
+      </div>
+      <div className='challenge-header'>
+        <a href="/perfil">
+          <img src="https://picsum.photos/80" alt="" />``
+        </a>
+        <div className="ranking">
+          <div className="podio">2 <img src="https://picsum.photos/80" alt="" /></div>
+          <div className="podio primeiro">1 <img src="https://picsum.photos/80" alt="" /></div>
+          <div className="podio">3 <img src="https://picsum.photos/80/200" alt="" /></div>
+        </div>
+      </div>
+
+      <div className="posts-container">
+        <h3>-- 09/12/2024 --</h3>
+
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+        {PostCard()}
+
+      </div>
+
+      <a className='create-post' href="/desafio/[id]/post/criar"> <PlusSvg/> </a>
     </div>
   );
+}
+
+function PostCard(){
+  return(
+    <a href='/desafio/[id]/post/[postId]' className="postCard">
+      <img src="https://picsum.photos/80/400" alt="" />
+      <div className="postInfo">
+        <h4>Titulo do card</h4>
+        <div className="postUser">
+          <img src="https://picsum.photos/80/300" alt="" />
+          <p>Nome aaaaa</p>
+        </div>
+      </div>
+    </a>
+  )
 }
---
+++ src/app/desafio/[id]/post/[postId]/page.jsx
@@ -0,0 +1,32 @@
+import './post.css'
+import ReturnSvg from "@/components/svg/ReturnSvg";
+
+export const metadata = {
+  title: "Post - id",
+  description: "Página para a visualizar um Post",
+};
+
+export default async function Home({ params }) {
+  const { id, postId } = await params;
+
+  return (
+    <div className='post-container'>
+      <div className="header">
+        <a href="/"><ReturnSvg/></a>
+        <h1>Challenge Name</h1>
+      </div>
+      <div className="post-content">
+        <img src="https://picsum.photos/300/120" alt="" />
+        <div className="post-user">
+          <img src="https://picsum.photos/80" alt="" />
+          <div className="user-data">
+            <h3>Nome usuario</h3>
+            <p>09/12/2024</p>
+          </div>
+        </div>
+        <h2>Titulo do post</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni excepturi, magnam atque quos ab beatae fugiat molestias veritatis suscipit, maiores ut voluptatibus similique repellat eligendi natus. Pariatur cupiditate labore provident?</p>
+      </div>
+    </div>
+  );
+}
